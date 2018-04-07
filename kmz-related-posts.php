299ec760a2dbc8c46efaa0c71468a255d49ad4a6
Vladimir Kamuz <v.kamuz@gmail.com>
2018-04-07 12:21:47 +0300

    Output links of related posts on rand order

--- kmz-related-posts.php
+++ kmz-related-posts.php
@@ -14,8 +14,23 @@ function kmz_related_posts($content){
     foreach($categories as $category){
         $cats_id[] = $category->cat_ID;
     }
-    echo '<pre>';
-    print_r($cats_id);
-    echo '</pre>';
+
+    $related_posts = new WP_Query(
+        array(
+            'posts_per_page' => 5,
+            'category__in' => $cats_id,
+            'orderby' => 'rand'
+        )
+    );
+
+    if($related_posts->have_posts()){
+        $content .= '<div class="related-posts"><h3>Maybe you interested</h3>';
+        while($related_posts->have_posts()){
+            $related_posts->the_post();
+            $content .= '<a href="' . get_the_permalink() . '">' . get_the_title() . '</a><br>';
+        }
+        $content .= '</div>';
+    }
+
     return $content;
 }
