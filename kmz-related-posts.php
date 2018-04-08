01fb3bb2304a8c405e035d5fe758b0a149b1c7d0
Vladimir Kamuz <v.kamuz@gmail.com>
2018-04-08 21:01:54 +0300

    Added thumbnails and tooltips on hover

--- kmz-related-posts.php
+++ kmz-related-posts.php
@@ -6,6 +6,18 @@
  * Plugin URL: https://kamuz.pro
  */
 
+function kmz_related_enqueue_style() {
+    wp_enqueue_style( 'kmz-related-style', plugins_url( 'css/', __FILE__ ).'main.css', false ); 
+}
+
+function kmz_related_enqueue_script() {
+    wp_enqueue_script( 'kmz-related-tooltips',  plugins_url( 'js/', __FILE__ ).'jquery.tools.min.js', array('jquery') );
+    wp_enqueue_script( 'kmz-related-script',  plugins_url( 'js/', __FILE__ ).'main.js', false );
+}
+
+add_action( 'wp_enqueue_scripts', 'kmz_related_enqueue_style' );
+add_action( 'wp_enqueue_scripts', 'kmz_related_enqueue_script' );
+
 add_filter('the_content', 'kmz_related_posts');
 
 function kmz_related_posts($content){
@@ -20,7 +32,7 @@ function kmz_related_posts($content){
 
     $related_posts = new WP_Query(
         array(
-            'posts_per_page' => 5,
+            'posts_per_page' => 4,
             'category__in' => $cats_id,
             'orderby' => 'rand',
             'post__not_in' => array($id)
@@ -31,7 +43,12 @@ function kmz_related_posts($content){
         $content .= '<div class="related-posts"><h3>Maybe you interested:</h3>';
         while($related_posts->have_posts()){
             $related_posts->the_post();
-            $content .= '<a href="' . get_the_permalink() . '">' . get_the_title() . '</a><br>';
+            if(has_post_thumbnail()){
+                $img = get_the_post_thumbnail( get_the_ID(), array(100, 100), array( 'title' => get_the_title(), 'alt' => get_the_title() ) );
+            }else{
+                $img = '<img src="' . plugins_url('img/no_img.jpg', __FILE__) . '" alt="' . get_the_title() . '" title="' . get_the_title() . '" width="100" height="100">';
+            }
+            $content .= '<a href="' . get_the_permalink() . '">' . $img . '</a>';
         }
         $content .= '</div>';
         wp_reset_query();
